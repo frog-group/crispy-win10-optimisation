dfad91196bfa2f9e7800d94e16865d5a0e434a17
usbhub95 <43819486+usbhub95@users.noreply.github.com>
2020-12-30 16:02:02 +1100

    Add files via upload

---
+++ download-software.bat
@@ -0,0 +1,3 @@
+powershell -Command "Invoke-WebRequest https://coderbag.com/assets/downloads/cpm/currentversion/QuickCpuSetup64.zip -OutFile QuickCpuSetup64.zip"
+powershell -Command "Invoke-WebRequest https://download2266.mediafire.com/fmk43ym5410g/ewpy1p0rr132thk/MSI_util_v3.zip -OutFile MSI_util_v3.zip"
+powershell -Command "Invoke-WebRequest https://www.speedguide.net/files/TCPOptimizer.exe -OutFile TCPOptimizer.exe"
