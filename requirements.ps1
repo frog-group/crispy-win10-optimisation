66ece2705b5c16037a06163d464e55dc3bc8c16a
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2021-09-09 23:45:43 +1000

    update software links

--- requirements.ps1
+++ requirements.ps1
@@ -33,16 +33,16 @@ $gitRepos = @{
 $softwareLinks = @{
     'QuickCpuSetup64.zip' = 'https://coderbag.com/assets/downloads/cpm/currentversion/QuickCpuSetup64.zip'
     'TCPOptimizer.exe' = 'https://www.speedguide.net/files/TCPOptimizer.exe'
-    'DDU v18.0.4.2.exe' = 'https://www.wagnardsoft.com/DDU/download/DDU v18.0.4.2.exe'
-    'ISLC v1.0.2.4.exe' = 'https://www.wagnardsoft.com/ISLC/ISLC v1.0.2.4.exe'
+    'DDU v18.0.4.3.exe' = 'https://www.wagnardsoft.com/DDU/download/DDU v18.0.4.3.exe'
+    'ISLC v1.0.2.5.exe' = 'https://www.wagnardsoft.com/ISLC/ISLC v1.0.2.5.exe'
     'privatezilla.zip' = 'https://github.com/builtbybel/privatezilla/releases/download/0.50.0/privatezilla.zip'
 }
 
 #FORMAT:
 #   'SFX NAME (OMITTING EXTENSION)'
 $sfxes = @(
-    'DDU v18.0.4.2'
-    'ISLC v1.0.2.4'
+    'DDU v18.0.4.3'
+    'ISLC v1.0.2.5'
 )
 
 # define useful funcs
